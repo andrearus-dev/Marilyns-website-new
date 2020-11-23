cd51352ead6584bec0d815bcf33b896cb8ca366c
Andrea Rus <andrearus27@gmail.com>
2020-11-23 11:40:30 +0000

    Update script.js

--- Marilyns-Website-master/script.js
+++ Marilyns-Website-master/script.js
@@ -19,10 +19,13 @@ const form = document.querySelector('.contact-form');
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
+const smallTag = document.getElementsByTagName('small');
 
 function showError(input, message) {
 
     
+    smallTag.className = small.show;
+
 }
 
 
@@ -32,4 +35,9 @@ form.addEventListener('submit', function(e) {
 
     e.preventDefault();
 
+    if(name.value === '') {
+        showError(name, 'Name is required');
+    } else {
+        showSuccess(name);
+    }
 })
