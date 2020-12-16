efa5e7d1140d0b43e778940c171b1af96c27722e
Andrea Rus <andrearus27@gmail.com>
2020-12-16 10:24:34 +0000

    Update script.js

--- Marilyns-Website-master/script.js
+++ Marilyns-Website-master/script.js
@@ -27,7 +27,7 @@ function showError(input, message) {
 
     const formControl = input.parentElement;
     formControl.className = 'form-control error';
-    const small = document.querySelector('small');
+    const small = formControl.querySelector('small');
     small.innerText = message;
 }
 
